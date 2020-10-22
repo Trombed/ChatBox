ba8ea15192676deeabd888b545b9ad094ecc402d
Eric Ho <eric.h924@gmail.com>
2020-10-21 18:49:41 -0700

    fix years rendering

--- features/technology.js
+++ features/technology.js
@@ -3,14 +3,17 @@ const data = require('./data/data.js')
 
 module.exports = function(controller) {
     var years = (new Date().getFullYear()) - data.techYears;
+    console.log(years)
+
     let plural = (years > 1 ? "years" : "year")
     var tech = data.techs.map( tech => tech.title.toLowerCase())
     let knownTech = new Set(tech)
-   
+
     controller.hears(["Rechart"],'message,direct_message', async(bot,message)=> {
+  
         await bot.reply(message, 
             { 
-                text: `Recharts is a light weight chart visualization library. ${data.firstName} has used it for ${data.techYears}`,
+                text: `Recharts is a light weight chart visualization library. ${data.firstName} has used it for ${years} ${plural}`,
                 quick_replies: data.techs
             })
     })
@@ -28,23 +31,27 @@ module.exports = function(controller) {
             })
     })
 
+    const capitals = new Set(["SQL", "HTML", "CSS"])
 
     controller.hears(tech,'message,direct_message', async(bot,message)=> {
         let cur;
         message.text.split(" ").forEach( word => {
 
             if (knownTech.has(word.toLowerCase())) {
-                cur = word;
-                cur = cur.charAt(0).toUpperCase() + cur.slice(1).toLowerCase();
+                if (capitals.has(word.toUpperCase())) {
+                    cur = word.toUpperCase()
+                } else {   
+                    cur = word;
+                    cur = cur.charAt(0).toUpperCase() + cur.slice(1).toLowerCase();
+                }
             } 
         })
         await bot.reply(message, 
             { 
-                text: `${data.firstName} has experience with ${cur} for ${plural}.`,
+                text: `${data.firstName} has experience with ${cur} for ${years} ${plural}.`,
                 quick_replies: data.techs
             }
         )
     })
-
 
 }
--- public/script.js
+++ public/script.js
@@ -1,13 +1,17 @@
 let menu = document.getElementById('help')
 let readme = document.getElementById('readme')
 let close = document.getElementById('close')
+let exitButton = document.getElementById("exit_button")
+
+
 
 menu.onclick = function() {
-    if (readme.style.display === "none") {
-        readme.style.display = "block";
-      } else {
-        readme.style.display = "none";
-      }
+  if (readme.style.display === "none" || readme.style.display === "") {
+    readme.style.display = "block"
+  } else {
+    readme.style.display = 'none'
+  }
+
 }
 
 document.addEventListener ('keyup', (e) => {
@@ -17,5 +21,10 @@ document.addEventListener ('keyup', (e) => {
 })
 
 close.onclick = function() {
-      readme.style.display = "none";
+  readme.style.display = "none"
 }
+
+exitButton.onclick = function() {
+  readme.style.display = "none"
+}
+
